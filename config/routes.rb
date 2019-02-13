9a7b47edf13e888b12e17e9ac6aa468189fecf33
Shirakawa-Satoshi <shirakawasatoshi@shirakawasatoshinoMacBook-Air.local>
2019-02-13 12:17:34 +0900

    Set routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
 
+  devise_for :users
+  resources :users, only: :show
+  resources :products, only: :show do
+    resources :reviews, only: [:new, :create]
+  end
+  root 'products#index'
 end
