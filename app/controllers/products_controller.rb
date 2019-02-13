efa7ed350e3d6a01f6b57c5ddd7ee5f35460db50
Shirakawa-Satoshi <shirakawasatoshi@shirakawasatoshinoMacBook-Air.local>
2019-02-13 21:31:14 +0900

    Create products#show

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,9 @@ class ProductsController < ApplicationController
   def index
     @products = Product.order("date DESC").page(params[:page]).per(12)
   end
+
+  def show
+    @product = Product.find(params[:id])
+  end
 end
+
