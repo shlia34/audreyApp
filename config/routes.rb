cf6e1cbb344e8fc05067b5bdd38633f02324159c
Shirakawa-Satoshi <shirakawasatoshi@shirakawasatoshinoMacBook-Air.local>
2019-02-14 14:33:32 +0900

    Modify rotes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: :show
   resources :products, only: :show do
-    resources :reviews, only: [:new, :create]
+    resources :reviews, only: [:create]
   end
   root 'products#index'
 end
