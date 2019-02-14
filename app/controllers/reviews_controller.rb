9ca9c8def98d2be79fcd408933c328c281f4a42e
Shirakawa-Satoshi <shirakawasatoshi@shirakawasatoshinoMacBook-Air.local>
2019-02-14 16:15:59 +0900

    Enable to save reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,15 @@
 class ReviewsController < ApplicationController
+
+  def create
+    @review = Review.create(text: review_params[:text],
+                            product_id: review_params[:product_id],
+                            user_id: current_user.id)
+    redirect_to "/products/#{@review.product.id}"
+  end
+
+  private
+  def review_params
+    params.permit(:text, :product_id)
+  end
+
 end
