4f865c3ea2d38101aaa413fb39e83567f6ba1abf
Shirakawa-Satoshi <shirakawasatoshi@shirakawasatoshinoMacBook-Air.local>
2019-02-14 17:50:30 +0900

    Create "users/show"

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  resources :users, only: :show
+  resources :users, only: [:index, :show]
   resources :products, only: :show do
     resources :reviews, only: [:create]
   end
