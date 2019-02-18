dbc4829e69741d691c25b8b0b7e7456e5d879742
Shirakawa-Satoshi <shirakawasatoshi@shirakawasatoshinoMacBook-Air.local>
2019-02-18 17:33:06 +0900

    Enable search products by date

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,8 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.order("date DESC").page(params[:page]).per(12)
+    @q = Product.search(params[:q])
+    @products = @q.result(distinct: true).order("date DESC").page(params[:page]).per(12)
   end
 
   def show
