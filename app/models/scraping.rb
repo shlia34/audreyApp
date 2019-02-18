3f0a18f54fb56f57f0767e67e37f558e2f91e3bc
Shirakawa-Satoshi <shirakawasatoshi@shirakawasatoshinoMacBook-Air.local>
2019-02-19 01:07:21 +0900

    Scraping other site

--- app/models/scraping.rb
+++ app/models/scraping.rb
@@ -1,5 +1,5 @@
 class Scraping
-  def self.get_urls
+  def self.get_urls_a
     links = []
     agent = Mechanize.new
     current_page = agent.get("http://www.allnightnippon.com/kw/")
@@ -9,11 +9,11 @@ class Scraping
     end
 
     links.each do |link|
-      get_product(link)
+      get_product_a(link)
     end
   end
 
-  def self.get_product(link)
+  def self.get_product_a(link)
     agent     = Mechanize.new
     page      = agent.get(link)
     title     = page.at('.sp_title').inner_text
@@ -21,7 +21,38 @@ class Scraping
     url       = page.at('.blog_text-area img')[:src]
     image_url = "http://www.allnightnippon.com" + url
 
-    product = Product.new(title: title, date: date, image_url: image_url, link: link)
+    product = Product.where(title: title, date: date, image_url: image_url, link: link).first_or_initialize
     product.save
   end
+
+   def self.get_urls_b
+    links = []
+    agent = Mechanize.new
+    current_page = agent.get("http://www.allnightnippon.com/kw_past/backnumber.php")
+    elements = current_page.search('.title h2 a')
+    elements.each do |ele|
+      link = ele.get_attribute('href').sub(/./, 'http://www.allnightnippon.com/kw_past')
+      links << link
+    end
+
+    links.each do |link|
+      get_product_b(link)
+    end
+  end
+
+  def self.get_product_b(link)
+    agent     = Mechanize.new
+    page      = agent.get(link)
+    title     = page.at('.title h2').inner_text
+    date      = page.at('.date').inner_text.to_s
+    if page.at('.articleImg img')
+      url       = page.at('.articleImg img')[:src]
+      image_url = "http://www.allnightnippon.com" + url
+    end
+
+    product = Product.where(title: title, date: date, image_url: image_url, link: link).first_or_initialize
+    product.save
+  end
+
 end
+
