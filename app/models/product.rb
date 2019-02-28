c1dc3f285f372bf1dace235460221919d6130c4c
Shirakawa-Satoshi <shirakawasatoshi@shirakawasatoshinoMacBook-Air.local>
2019-02-28 14:14:26 +0900

    Set association 'product' and 'user'

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,6 @@
 class Product < ApplicationRecord
   has_many :reviews
+
   has_many :likes, dependent: :destroy
-  def like_user(user_id)
-   likes.find_by(user_id: user_id)
-  end
+  has_many :liking_users, through: :likes, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   mount_uploader :image, ImageUploader
   has_many :reviews
+
+  has_many :likes, dependent: :destroy
+  has_many :like_tweets, through: :likes, source: :tweet
 end
