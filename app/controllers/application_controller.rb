b1e482771cc643c1002090f5e40876cd12cbef05
Shirakawa-Satoshi <shirakawasatoshi@shirakawasatoshinoMacBook-Air.local>
2019-03-01 01:09:22 +0900

    Remove basic Authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
+
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
@@ -9,10 +9,4 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :image, :profile])
   end
 
-  private
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == 'annkw' && password == 'annkw'
-    end
-  end
 end
