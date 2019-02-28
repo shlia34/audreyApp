ffadb53b181668ece0e2ea94df48e6b1b3844620
Shirakawa-Satoshi <shirakawasatoshi@shirakawasatoshinoMacBook-Air.local>
2019-03-01 00:50:43 +0900

    プリコンパイル通したい

--- config/application.rb
+++ config/application.rb
@@ -5,13 +5,13 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+config.assets.initialize_on_precompile = false
 
 module AudreyApp
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.assets.initialize_on_precompile = true
     config.generators do |g|
       g.stylesheets false
       g.javascripts false
