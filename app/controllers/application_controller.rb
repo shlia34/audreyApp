81ffda4f17dd344b74272d9284e5196e8fc20ab5
Shirakawa-Satoshi <shirakawasatoshi@shirakawasatoshinoMacBook-Air.local>
2019-03-10 15:12:31 +0900

    Basic認証　追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
 
+  before_action :basic_auth
+
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
@@ -7,6 +9,13 @@ class ApplicationController < ActionController::Base
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :image, :profile])
+  end
+
+  private
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'shira' && password == 'shira'
+    end
   end
 
 end
