2df00be6d79b64e72ec60709b0c4a59b2927670e
Shirakawa-Satoshi <shirakawasatoshi@shirakawasatoshinoMacBook-Air.local>
2019-03-11 20:33:51 +0900

    Set 'users#follow_list' and 'users#follower_list'

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show, :edit, :update]
   put 'users/follow/:user_id' => 'users#follow'
   put 'users/unfollow/:user_id' => 'users#unfollow'
+  get 'users/follow_list/:user_id' => 'users#follow_list'
+  get 'users/follower_list/:user_id' => 'users#follower_list'
   resources :products, only: :show do
     resources :reviews, only: [:create]
   end
