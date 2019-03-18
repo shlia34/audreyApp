8d36b4467682965b56a5e0bf1f20c75773fd0a55
Shirakawa-Satoshi <shirakawasatoshi@shirakawasatoshinoMacBook-Air.local>
2019-03-19 02:11:56 +0900

    Devisのストロングパラメーターを誤ってコメントアウトしていたのを修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,16 @@
 class ApplicationController < ActionController::Base
 
-  # before_action :basic_auth
 
-  # protect_from_forgery with: :exception
+  protect_from_forgery with: :exception
   before_action :authenticate_user!, except: [:index, :show, :follow_list, :follower_list]
-  # before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  # protected
-  # def configure_permitted_parameters
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :image, :profile])
-  # end
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :image, :profile])
+  end
+
+   # before_action :basic_auth
 
   # private
   # def basic_auth
