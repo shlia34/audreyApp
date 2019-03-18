5bd39e4ffa318a15c3267db7b8e0fe2d6b05ec26
Shirakawa-Satoshi <shirakawasatoshi@shirakawasatoshinoMacBook-Air.local>
2019-03-19 00:11:49 +0900

    Basic認証を解除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,21 @@
 class ApplicationController < ActionController::Base
 
-  before_action :basic_auth
+  # before_action :basic_auth
 
-  protect_from_forgery with: :exception
-  before_action :authenticate_user!
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  # protect_from_forgery with: :exception
+  # before_action :authenticate_user!
+  # before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :image, :profile])
-  end
+  # protected
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :image, :profile])
+  # end
 
-  private
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == 'shira' && password == 'shira'
-    end
-  end
+  # private
+  # def basic_auth
+  #   authenticate_or_request_with_http_basic do |username, password|
+  #     username == 'shira' && password == 'shira'
+  #   end
+  # end
 
 end
